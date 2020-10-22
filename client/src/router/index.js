e32657639275e0a36e09227c876cd68ab8abd9aa
BangBreakfast <glrglrglrglr@163.com>
2020-10-22 15:54:46 +0800

    insert element-ui

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import ElementUi from 'element-ui'
+import '@/theme-et/index.css'
+
+Vue.use(ElementUi)
+
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/test/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
