6380c83b72b12aa82322e25608fb60f16f268ee1
unknown <rebeating@163.com>
2020-12-12 21:55:59 +0800

    course

---
+++ elect_system/client/src/components/admin/adminCourseDetail.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <el-row :span="24"><h1>{{course.name}}</h1></el-row>
+        <el-row>
+            <el-col :span="12">课程编号</el-col><el-col :span="12">{{course.course_id}}</el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">课程类别</el-col>
+            <el-col :span="12">
+                {{Mainclass[course.main_class-1]}}
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">课程学分</el-col><el-col :span="12">{{course.credit}}</el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">开课院系</el-col><el-col :span="12">信科</el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">任课老师</el-col><el-col :span="12">{{course.lecturer}}</el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">上课地点</el-col><el-col :span="12">{{course.pos}}</el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">上课时间</el-col>
+            <el-col :span="6" v-for ="(time,timeIndex) in course.times" :key="timeIndex">
+                {{date[time.day-1]}}
+                <div v-for ="(lesson,lessonIndex) in time.period" :key="lessonIndex">
+                {{lessons[lesson-1]}}
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">详细描述</el-col><el-col :span="12">{{course.detail}}</el-col>
+        </el-row>
+    </div>
+</template>
+
+<style>
+  .el-row {
+    margin-bottom: 40px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+</style>
+
+<script>
+import axios from 'axios'
+export default {
+    mounted () {
+        //axios.get('http://localhost:8000/courses/'+this.$route.params.id, {}).then(response => (course = response.course))
+    },
+    data () {
+        return {
+            course: {
+                "course_id": 1233343,   // From elective.pku.edu.cn
+                // "class_no": 1,
+                "name": "软件工程",
+                "credit": 4,
+                "main_class": 1,     // 课程类别, 专业课、通选课、体育课等
+                "sub_class": null,   // 通选课类别(ABCDEF) / 英语课类别(ABCC+)，其它大类可缺省
+                "times": [
+                    {"day":2, "period":[3,4]},
+                    {"day":4, "period":[5,6]},
+                ],
+                "lecturer": "孙艳春",
+                "pos": "理教201",
+                "dept": 48,
+                "detail": "About software developing...",     // 详细的文字描述
+                "elect_status": 0,    // none / pending / elected
+                // "willpoint": 99, feature: 在详情页选课
+            },
+            lessons: [
+                '08:00-08:50',
+                '09:00-09:50',
+                '10:10-11:00',
+                '11:10-12:00',
+                '13:00-13:50',
+                '14:00-14:50',
+                '15:10-16:00',
+                '16:10-17:00',
+                '17:10-18:00',
+                '18:40-19:30',
+                '19:40-20:30',
+                '20:40-21:30'
+            ],
+            date: [
+                '周一',
+                '周二',
+                '周三',
+                '周四',
+                '周五',
+                '周六',
+                '周日'
+            ],
+            Mainclass: [
+                '专业课',
+                '政治课',
+                '体育课',
+                '英语课',
+                '通选课'
+            ]
+        }
+    }
+}
+</script>
---
+++ elect_system/client/src/components/admin/adminCourseEdit.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-row>{{course.name}}</v-row>
+        <v-row>
+            <v-col>课程ID</v-col><v-col><el-input type="textarea" :placeholder="course.course_id" v-model="course.course_id"></el-input></v-col>
+        </v-row>
+        <v-row>
+            <v-col>班号</v-col><v-col><el-input type="textarea" :placeholder="course.class_no" v-model="course.class_no"></el-input></v-col>
+        </v-row>
+        <v-row>
+            <v-col>学分</v-col><v-col><el-input type="textarea" :placeholder="course.credit" v-model="course.credit"></el-input></v-col>
+        </v-row>
+        <v-row>
+            <v-col>开课院系</v-col><v-col><el-input type="textarea" :placeholder="course.dept" v-model="course.dept"></el-input></v-col>
+        </v-row>
+        <v-row>
+            <v-col>任课老师</v-col><v-col><el-input type="textarea" :placeholder="course.lecturer" v-model="course.lecturer"></el-input></v-col>
+        </v-row>
+        <v-row>
+            <v-col>上课地点</v-col><v-col><el-input type="textarea" :placeholder="course.pos" v-model="course.pos"></el-input></v-col>
+        </v-row>
+        <v-row>
+            <v-col>上课时间</v-col><v-col><el-input type="textarea" :placeholder="course.times" v-model="course.times"></el-input></v-col>
+        </v-row>
+        <el-button type="primary" ></el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            course : {
+                'course_id': 1233343,
+                'class_no': 1,
+                'name': '软件工程',
+                'credit': 4,
+                'times': [
+                    {'day': 2, 'period': [3, 4]},
+                    {'day': 4, 'period': [5, 6]}
+                ],
+                'time': '周二吃早饭',
+                'lecturer': '孙艳春',
+                'pos': '理教201',
+                'dept': 48,
+                'election': {
+                    'status': 1,
+                    'willpoint': 99,
+                    'elected_num': 121,
+                    'capacity': 150,
+                    'pending_num': 13
+                }
+            }
+        }
+    }
+}
+</script>
