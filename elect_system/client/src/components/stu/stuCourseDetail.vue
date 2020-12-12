7cf2b5816b366a410c27114f314c5878011d3125
unknown <rebeating@163.com>
2020-12-12 22:09:03 +0800

    course

--- elect_system/client/src/components/stu/stuCourseDetail.vue
+++ elect_system/client/src/components/stu/stuCourseDetail.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <el-row :span="24"><h1>{{course.name}}</h1></el-row>
+        <el-row :span="24"><h1>{{this.course.name}}</h1></el-row>
         <el-row>
-            <el-col :span="12">课程编号</el-col><el-col :span="12">{{course.course_id}}</el-col>
+            <el-col :span="12">课程编号</el-col><el-col :span="12">{{this.course.course_id}}</el-col>
         </el-row>
         <el-row>
             <el-col :span="12">课程类别</el-col>
@@ -11,20 +11,20 @@
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="12">课程学分</el-col><el-col :span="12">{{course.credit}}</el-col>
+            <el-col :span="12">课程学分</el-col><el-col :span="12">{{this.course.credit}}</el-col>
         </el-row>
         <el-row>
             <el-col :span="12">开课院系</el-col><el-col :span="12">信科</el-col>
         </el-row>
         <el-row>
-            <el-col :span="12">任课老师</el-col><el-col :span="12">{{course.lecturer}}</el-col>
+            <el-col :span="12">任课老师</el-col><el-col :span="12">{{this.course.lecturer}}</el-col>
         </el-row>
         <el-row>
-            <el-col :span="12">上课地点</el-col><el-col :span="12">{{course.pos}}</el-col>
+            <el-col :span="12">上课地点</el-col><el-col :span="12">{{this.course.pos}}</el-col>
         </el-row>
         <el-row>
             <el-col :span="12">上课时间</el-col>
-            <el-col :span="6" v-for ="(time,timeIndex) in course.times" :key="timeIndex">
+            <el-col :span="6" v-for ="(time,timeIndex) in this.course.times" :key="timeIndex">
                 {{date[time.day-1]}}
                 <div v-for ="(lesson,lessonIndex) in time.period" :key="lessonIndex">
                 {{lessons[lesson-1]}}
@@ -32,7 +32,7 @@
             </el-col>
         </el-row>
         <el-row>
-            <el-col :span="12">详细描述</el-col><el-col :span="12">{{course.detail}}</el-col>
+            <el-col :span="12">详细描述</el-col><el-col :span="12">{{this.course.detail}}</el-col>
         </el-row>
     </div>
 </template>
@@ -50,7 +50,7 @@
 import axios from 'axios'
 export default {
     mounted () {
-        //axios.get('http://localhost:8000/courses/'+this.$route.params.id, {}).then(response => (course = response.course))
+        axios.get('http://localhost:8000/courses/'+this.$route.params.id, {}).then(response => (this.course = response.course))
     },
     data () {
         return {
