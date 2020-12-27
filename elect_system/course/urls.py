d6882b12705bbe458ca0ef7bed7e917129193797
jy5275 <527507046@qq.com>
2020-12-27 23:13:58 +0800

    not send dynamic data in courseDetail req

--- elect_system/course/urls.py
+++ elect_system/course/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('showallcourse', views.show_all_course),  # just for debug
 	path('courses', views.course),
 	path('dept', views.dept),
-	re_path(r'courses/(\d{0,})/detail', views.courseinfo),
+	re_path(r'courses/(\d{0,})/detail', views.courseDetail),
 	re_path(r'courses/(\d{0,})', views.course),
 
 ]
