7252f03018f6a44cc5ba4d0925f48989010f7ee0
jy5275 <527507046@qq.com>
2020-12-28 01:43:02 +0800

    refine password change api

--- elect_system/user/models.py
+++ elect_system/user/models.py
@@ -5,28 +5,6 @@ import random
 from threading import Lock
 
 
-class VerificationCode(models.Model):
-    email = models.EmailField(unique=True)
-    code = models.CharField(max_length=6)
-    genTime = models.DateTimeField()
-
-    def getVerificationCode(chMail: str):
-        codeGen = 0
-        if VerificationCode.objects.filter(email=chMail).exists():
-            vc = VerificationCode.objects.get(email=chMail)
-            if (timezone.now() - vc.genTime).total_seconds() < 30:
-                return vc.code
-            codeGen = random.randint(1000, 9999)
-            vc.code = codeGen
-            vc.genTime = timezone.now()
-            vc.save()
-        else:  # Does not exist
-            codeGen = random.randint(1000, 9999)
-            vc = VerificationCode(
-                email=chMail, code=codeGen, genTime=timezone.now())
-            vc.save()
-        return codeGen
-
 
 class Message(models.Model):
     genTime = models.DateTimeField(null=False)
@@ -56,5 +34,29 @@ class User(django.contrib.auth.models.User):
             str(self.willingpointLimit) + '>'
 
 
-
 stuLock = {}
+
+class VerificationCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    code = models.CharField(max_length=6)
+    genTime = models.DateTimeField()
+
+    def getVerificationCode(u: User):
+        codeGen = 0
+        vSet = VerificationCode.objects.filter(user=u.id)
+
+        if vSet.exists():
+            v = vSet.get()
+            # Not allowed to get diff vcode within 30 seconds
+            if (timezone.now() - v.genTime).total_seconds() < 30:
+                return v.code
+            codeGen = random.randint(1000, 9999)
+            v.code = codeGen
+            v.genTime = timezone.now()
+            v.save()
+        else:  # Does not exist
+            codeGen = random.randint(1000, 9999)
+            vc = VerificationCode.objects.create(
+                user=u, code=codeGen, genTime=timezone.now())
+            vc.save()
+        return codeGen
